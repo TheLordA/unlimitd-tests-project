bcf74bb268a1a3929b677712a8bce9331e646c1a
Anass FERRAK <an.ferrak@gmail.com>
2023-03-06 09:59:35 +0100

    feat : add AuthContext logic

--- client-authentication/src/contexts/AuthContext.js
+++ client-authentication/src/contexts/AuthContext.js
@@ -5,38 +5,49 @@ import { CURRENT_USER } from 'src/graphql/users/queries';
 const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
-  // eslint-disable-next-line no-unused-vars
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isInitialized, setIsInitialized] = useState(false);
 
   const logout = () => {
-    // TODO: Implement the logout logic
+    localStorage.removeItem('token');
+    setIsAuthenticated(false);
   };
 
-  // eslint-disable-next-line no-unused-vars
   const [getCurrentUser, { data: currentUser, refetch: refetchCurrentUser }] = useLazyQuery(
     CURRENT_USER,
     {
       nextFetchPolicy: 'cache-and-network',
       onCompleted: () => {
-        // TODO: Implement on completed logic
+        setIsAuthenticated(true);
       },
       onError: () => {
-        // TODO: Implement on error logic
+        localStorage.removeItem('token');
+        setIsAuthenticated(false);
       },
       refetchWritePolicy: 'overwrite',
     }
   );
 
-  // eslint-disable-next-line no-unused-vars
   const onLoginSuccess = (token) => {
-    // TODO: Implement the onLoginSuccess logic
+    localStorage.setItem('token', token);
+    getCurrentUser();
   };
 
   useEffect(() => {
-    // TODO: implement the initialization logic
-    setIsInitialized(true);
+    const token = localStorage.getItem('token');
+    if (token) {
+      getCurrentUser();
+    } else {
+      setIsInitialized(true);
+    }
   }, []);
+
+  useEffect(() => {
+    if (currentUser && currentUser.me) {
+      setIsAuthenticated(true);
+    }
+    setIsInitialized(true);
+  }, [currentUser]);
 
   return (
     <AuthContext.Provider
