0e8b06e8707a89ee0f495972ff23b0c110cc835a
Anass FERRAK <an.ferrak@gmail.com>
2023-03-06 10:18:41 +0100

    feat : add RoleBasedGuard logic

--- client-authentication/src/guards/RoleBasedGuard.js
+++ client-authentication/src/guards/RoleBasedGuard.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 
-// TODO: implement the role based guard logic
-// eslint-disable-next-line no-unused-vars
-const RoleBasedGuard = ({ accessibleRoles, children }) => <>{children}</>;
+import LoadingScreen from '../components/LoadingScreen';
+import useAuth from '../hooks/useAuth';
+
+// Helper function to check if the user has the required role
+const hasRole = (user, accessibleRoles) => {
+  if (!user || !user.roles || user.roles.length === 0) {
+    return false;
+  }
+  return accessibleRoles === user.role;
+};
+
+const RoleBasedGuard = ({ accessibleRoles, children }) => {
+  const { isAuthenticated, isInitialized, user } = useAuth();
+
+  // Show a loading screen while auth state is not initialized
+  if (!isInitialized) {
+    return <LoadingScreen />;
+  }
+
+  // Navigate to login if user is not authenticated
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  // Navigate to home if user does not have the required role
+  if (hasRole(user, accessibleRoles)) {
+    return <Navigate to="/" />;
+  }
+
+  // Allow access to the component if user is authenticated and has the required role
+  return <>{children}</>;
+};
 
 export default RoleBasedGuard;
