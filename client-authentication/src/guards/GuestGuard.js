40f50247b7d4f6d902c601681b3fee09995f9b07
Anass FERRAK <an.ferrak@gmail.com>
2023-03-06 10:03:37 +0100

    feat : add GuestGard logic

--- client-authentication/src/guards/GuestGuard.js
+++ client-authentication/src/guards/GuestGuard.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 
-// TODO: Implement the guest guard logic
-const GuestGuard = ({ children }) => <>{children}</>;
+import useAuth from '../hooks/useAuth';
+
+const GuestGuard = ({ children }) => {
+  const { isAuthenticated, isInitialized } = useAuth();
+
+  if (!isInitialized) {
+    return null;
+  }
+
+  if (isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+
+  return <>{children}</>;
+};
 
 export default GuestGuard;
