fb2125d04f4927579b512acf3a9354392d46bcca
Anass FERRAK <an.ferrak@gmail.com>
2023-03-06 10:40:51 +0100

    feat : add redirection depending on user role

--- client-authentication/src/pages/private/UserRoot.js
+++ client-authentication/src/pages/private/UserRoot.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import USER_ROLES from 'src/constants/userRoles';
+import useAuth from 'src/hooks/useAuth';
 
-// TODO: Implement user routing
-const UserRoot = () => <div>UserRoot</div>;
+const UserRoot = () => {
+  const navigate = useNavigate();
+  const { isInitialized, user } = useAuth();
+
+  useEffect(() => {
+    if (isInitialized) {
+      if (user && user.role === USER_ROLES.ADMIN) {
+        navigate('/admin');
+      } else if (user && user.role === USER_ROLES.USER) {
+        navigate('/user');
+      }
+    }
+  }, [isInitialized, user, navigate]);
+
+  return null;
+};
 
 export default UserRoot;
